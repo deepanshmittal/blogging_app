fba62dea8fab620ff700c1d68387d5afd53e942a
Deepansh <deepansh.mittal09@gmail.com>
2023-06-09 00:49:53 +0530

    updates for cookies

--- backend/api/views.py
+++ backend/api/views.py
@@ -46,8 +46,7 @@ class LoginUser(APIView):
                 }
                 response = Response()
                 response.data = user_data
-                response.set_cookie(key='jwt', value=token, httponly=True, domain='.pages.dev',
-                                    samesite=None)
+                response.set_cookie(key='jwt', value=token, httponly=True, samesite=None)
                 return response
 
 
