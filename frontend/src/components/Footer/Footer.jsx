28683ac1aa88b782242e1b220de9feac4adadaa8
Deepansh <deepansh.mittal09@gmail.com>
2023-06-09 13:40:42 +0530

    final commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -11,13 +11,19 @@ const Footer = () => {
         <footer>
             <img src={blog_logo} alt="" />
             <div className="social-links">
-                    <a href="https://www.linkedin.com/in/deepansh-mittal-17bba51bb/">
-                        <img src={linkedin} alt="" />
-                    </a>
-                    <a href="https://codeforces.com/profile/deepansh09"><img src={codeforces} alt="" /></a>
-                    <a href="https://leetcode.com/deepanshmittal09/"><img src={leetcode} alt="" /></a>
-                    <a href="https://github.com/deepanshmittal"><img src={github} alt="" /></a>
-                </div>
+                <a href="https://www.linkedin.com/in/deepansh-mittal-17bba51bb/">
+                    <img src={linkedin} alt="" />
+                </a>
+                <a href="https://codeforces.com/profile/deepansh09">
+                    <img src={codeforces} alt="" />
+                </a>
+                <a href="https://leetcode.com/deepanshmittal09/">
+                    <img src={leetcode} alt="" />
+                </a>
+                <a href="https://github.com/deepanshmittal/blogging_app">
+                    <img src={github} alt="" />
+                </a>
+            </div>
             <span>
                 Developed by <b>Deepansh Mittal</b>
             </span>
