e1f8648751114879de9f175ce599fd0c164445d9
Deepansh <deepansh.mittal09@gmail.com>
2023-06-11 00:42:24 +0530

    final commit

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.scss";
 import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
 import { API_URL } from "../../App";
+import GridLoader from "react-spinners/GridLoader";
+
 // const posts = [
 //     {
 //         id: 1,
@@ -38,15 +40,17 @@ import { API_URL } from "../../App";
 
 const Home = () => {
     const [posts, setPosts] = useState([]);
-
+    const [loader, setLoader] = useState(false);
     const cat = useLocation().search;
     // console.log(cat)
 
     useEffect(() => {
         const fetchData = async () => {
             try {
+                setLoader(true);
                 const response = await axios.get(`${API_URL}/posts${cat}`);
                 setPosts(response.data);
+                setLoader(false);
             } catch (err) {
                 console.log(err);
             }
@@ -62,27 +66,41 @@ const Home = () => {
     return (
         <div className="home">
             <div className="posts">
-                {posts.map((post) => (
-                    <div className="post" key={post.id}>
-                        <div className="img">
-                            <img src={post.blog_img_url} alt="" />
-                        </div>
-                        <div className="content">
-                            <Link className="link" to={`/post/${post.id}`}>
-                                <h1>{post.title}</h1>
-                            </Link>
-                            <p>
-                                <span className="limit">
-                                    {getText(post.description)}
-                                </span>
-                            </p>
-
-                            <Link className="link" to={`/post/${post.id}`}>
-                                <button>Read More...</button>
-                            </Link>
-                        </div>
+                {loader ? (
+                    <div className="GridLoader">
+                        <GridLoader
+                            color="#36d6cd"
+                            margin={30}
+                            size={50}
+                            speedMultiplier={1}
+                            width={100}
+                        />
+                        <h1>Loading Data from Server...</h1>
+                        <p>It might take upto 2 minutes for server to start</p>
                     </div>
-                ))}
+                ) : (
+                    posts.map((post) => (
+                        <div className="post" key={post.id}>
+                            <div className="img">
+                                <img src={post.blog_img_url} alt="" />
+                            </div>
+                            <div className="content">
+                                <Link className="link" to={`/post/${post.id}`}>
+                                    <h1>{post.title}</h1>
+                                </Link>
+                                <p>
+                                    <span className="limit">
+                                        {getText(post.description)}
+                                    </span>
+                                </p>
+
+                                <Link className="link" to={`/post/${post.id}`}>
+                                    <button>Read More...</button>
+                                </Link>
+                            </div>
+                        </div>
+                    ))
+                )}
             </div>
         </div>
     );
